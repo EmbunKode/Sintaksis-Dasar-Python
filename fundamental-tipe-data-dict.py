79990b59fa5cb1e78499882544748714b978af4f
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-07-26 22:04:30 +0700

    tipe data dictionary Python

--- fundamental-tipe-data-dict.py
+++ fundamental-tipe-data-dict.py
@@ -8,3 +8,21 @@ kamus_indo_eng = {'anak': 'son', 'istri': 'wave', 'ayah': 'father', 'ibu': 'moth
 print(kamus_indo_eng)
 print(kamus_indo_eng['ayah'])
 print(kamus_indo_eng['ibu'])
+
+print('Data ini dikirimkan oleh server Gojek, untuk memberikan info driver di sekitar pemakai aplikasi')
+data_dari_server_gojek = {
+    'tanggal': '2020-05-23',
+    'driver_list': [
+        {'nama': 'Sigit', 'jarak': 10},
+        {'nama': 'Wasis', 'jarak': 2000},
+        {'nama': 'Subekti', 'jarak': 200},
+    ]
+}
+
+print(data_dari_server_gojek)
+print(f"Driver disekitar sini {data_dari_server_gojek['driver_list']}")
+print(f"Driver #1 {data_dari_server_gojek['driver_list'][0]}")
+print(f"Driver #1 {data_dari_server_gojek['driver_list'][2]}")
+print(f"Driver #1 {data_dari_server_gojek['driver_list'][1]}")
+# mengambil jarak dari driver
+print(f"Jarak driver adalah {data_dari_server_gojek['driver_list'][0]['jarak']} meter")
