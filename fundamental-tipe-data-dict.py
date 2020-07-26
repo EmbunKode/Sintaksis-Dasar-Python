ecf2bc0c8d5ed4d299a891742625c6c66b94667c
sigit wasis subekti <sigitghoticmetal2001@gmail.com>
2020-07-26 17:12:18 +0700

    Tipe data Python

---
+++ fundamental-tipe-data-dict.py
@@ -0,0 +1,10 @@
+"""
+Tipe data dictionary sekedata menghubungkan antara KEY dan VALUE
+KVP = Key Value Pair
+"""
+
+kamus_indo_eng = {'anak': 'son', 'istri': 'wave', 'ayah': 'father', 'ibu': 'mother'}
+
+print(kamus_indo_eng)
+print(kamus_indo_eng['ayah'])
+print(kamus_indo_eng['ibu'])
